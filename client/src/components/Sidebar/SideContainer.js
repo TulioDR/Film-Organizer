8171caf105820a4df14b6038d845fec8a138126f
Tulio <tulioruzo29@gmail.com>
2022-06-01 22:46:07 -0400

    sidebar container reduced

--- client/src/components/Sidebar/SideContainer.js
+++ client/src/components/Sidebar/SideContainer.js
@@ -3,18 +3,10 @@ import useSidebarExtendedContext from "../../context/SidebarExtendedContext";
 export default function SideContainer({ children }) {
    const { sidebarExtended, sidebarRevealed } = useSidebarExtendedContext();
    return (
-      <div
-         className={`fixed md:pl-6 md:pb-6 pt-14 md:pt-24 top-0 left-0 h-full z-20 transform md:translate-x-0 duration-150 ${
-            sidebarRevealed ? "translate-x-0" : "-translate-x-full"
-         }`}
+      <aside
+         className={`fixed pt-20 top-0 left-0 h-full z-20 w-56 text-black flex flex-col`}
       >
-         <aside
-            className={`bg-purple-dark dark:bg-gray-lightDark text-white h-full duration-100 flex flex-col z-10 shadow-material md:rounded-lg ${
-               sidebarExtended ? "w-56" : "w-14 hover:delay-300 hover:w-60"
-            }`}
-         >
-            {children}
-         </aside>
-      </div>
+         {children}
+      </aside>
    );
 }
