2a5728a2d141c1d1d14da04cbd3342dc7e5bf679
Tulio <tulioruzo29@gmail.com>
2022-06-01 23:19:56 -0400

    changed links styles

--- client/src/components/Sidebar/SideContainer.js
+++ client/src/components/Sidebar/SideContainer.js
@@ -1,10 +1,10 @@
-import useSidebarExtendedContext from "../../context/SidebarExtendedContext";
+// import useSidebarExtendedContext from "../../context/SidebarExtendedContext";
 
 export default function SideContainer({ children }) {
-   const { sidebarExtended, sidebarRevealed } = useSidebarExtendedContext();
+   // const { sidebarExtended, sidebarRevealed } = useSidebarExtendedContext();
    return (
       <aside
-         className={`fixed pt-20 top-0 left-0 h-full z-20 w-56 text-black flex flex-col`}
+         className={`fixed pt-20 top-0 left-0 h-full z-20 w-60 text-black flex flex-col`}
       >
          {children}
       </aside>
--- client/src/components/Sidebar/SideItem.js
+++ client/src/components/Sidebar/SideItem.js
@@ -1,27 +1,40 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useRef } from "react";
 import useSidebarExtendedContext from "../../context/SidebarExtendedContext";
+import { matchPath } from "react-router";
 
 export default function SideItem({ name, icon, link, ...props }) {
    const buttonRef = useRef();
 
    const { closeSidebarMobile } = useSidebarExtendedContext();
-
+   const location = useLocation();
+   const match = matchPath(location.pathname, {
+      path: link,
+      exact: true,
+      strict: false,
+   });
    return (
       <li
-         className="h-11 relative group overflow-hidden hover:bg-purple dark:hover:bg-gray-light"
+         className="h-11 relative group overflow-hidden"
          onClick={closeSidebarMobile}
          ref={buttonRef}
       >
+         <div
+            className={`h-full rounded-r-full transform duration-200 ${
+               match?.isExact
+                  ? " w-full bg-blue-400"
+                  : " w-20 bg-blue-300 -translate-x-full group-hover:translate-x-0"
+            }`}
+         ></div>
          {props.children}
          <NavLink
             exact
             to={link}
-            activeClassName="bg-purple dark:bg-gray-light pointer-events-none"
-            className="cursor-pointer flex items-center h-full w-full px-2 "
+            // activeClassName="bg-blue-400 dark:bg-gray-light pointer-events-none rounded-r-full"
+            className="absolute left-0 top-0 pl-8 cursor-pointer flex items-center h-full w-full px-2"
          >
             <span className="material-icons mx-2">{icon}</span>
-            <span className="pl-2 truncate">{name}</span>
+            <span className="pl-4 truncate">{name}</span>
          </NavLink>
       </li>
    );
