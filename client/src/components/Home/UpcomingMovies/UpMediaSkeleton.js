26006bee1b65ab15e9c59f774d3fa247b9b0c34a
Tulio <tulioruzo29@gmail.com>
2022-06-30 19:59:32 -0400

    upcoming movies skeleton refactor

--- client/src/components/Home/UpcomingMovies/UpMediaSkeleton.js
+++ client/src/components/Home/UpcomingMovies/UpMediaSkeleton.js
@@ -1,9 +1,10 @@
 export default function UpMediaSkeleton() {
    return (
-      <div className="w-72 h-44 bg-gray-300 animate-pulse relative rounded-md">
-         <div className="absolute bottom-0 w-full p-2">
-            <div className="bg-gray-400 w-36 h-5 rounded-sm mb-1"></div>
-            <div className="bg-gray-400 w-44 h-5 rounded-sm"></div>
+      <div>
+         <div className="w-72 h-44 bg-gray-300 animate-pulse relative rounded-md"></div>
+         <div className="w-full mt-2">
+            <div className="bg-gray-400 w-2/3 h-4 rounded-sm mb-1"></div>
+            <div className="bg-gray-400 w-1/2 h-4 rounded-sm"></div>
          </div>
       </div>
    );
